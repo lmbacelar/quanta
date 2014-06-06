49ff6739dd18a45ba052ec8f513244834e746178
Luis Bacelar <lmbacelar@gmail.com>
2014-06-06 03:16:08 +0100

    refactored code. Operators *, / and ** result in unnamed dimension.

--- dimension.rb
+++ dimension.rb
@@ -4,7 +4,7 @@ class Dimension
   # Instance Methods
   #
   attr_accessor :label, :name, :symbol, :dimensions
-  def initialize label, name: label.to_s, symbol: label.to_s.upcase, dimensions: nil
+  def initialize label = nil, name: label.to_s, symbol: label.to_s.upcase, dimensions: nil
     raise TypeError, 'dimensions must be nil or a Hash' unless dimensions.nil? || 
                                                                dimensions.is_a?(Hash)
     @label      = label
@@ -12,6 +12,13 @@ class Dimension
     @symbol     = symbol
     @dimensions = dimensions || { self => 1 }
     freeze
+  end
+
+  def to_s
+    return symbol if base?
+    dimensions.map do |dimension, index|
+      index == 1 ? dimension.symbol : "#{dimension.symbol}^#{index}"
+    end.join ' . '
   end
 
   def base?
@@ -26,22 +33,13 @@ class Dimension
     dimensions == {}
   end
 
-  def to_s
-    return symbol if base?
-    return dimensions.map do |dimension, index|
-      index == 1 ? dimension.symbol : "#{dimension.symbol}^#{index}"
-    end.join ' . '
-  end
-
   def == other
     hash == other.hash
   end
 
   def hash
     [Dimension, label, name, symbol].concat(
-      Array(dimensions).map { |dimension, pow| 
-        [dimension.label, dimension.name, dimension.symbol, pow] 
-      }
+      Array(dimensions).map { |dim, pow| [dim.label, dim.name, dim.symbol, pow] }
     ).hash
   end
   alias_method :eql?, :==
@@ -50,78 +48,44 @@ class Dimension
     dimensions.hash == other.dimensions.hash
   end
 
+  def reciprocal
+    Dimension.new dimensions: Hash[dimensions.map{ |k, v| [k, v*-1] }]
+  end
+
+  def * other
+    multiply_or_divide :*, other
+  end
+
   def / other
     multiply_or_divide :/, other
   end
 
-  def * other
-    other = Dimension.one if other.is_a? Numeric
-    raise TypeError unless other.is_a? Dimension
+  def multiply_or_divide operator, other
+    return self.clone        if other.is_a? Numeric
+    raise TypeError          unless other.is_a? Dimension
+    return self.clone        if other.one?
+    other = other.reciprocal if operator == :/
+    return other.clone       if self.one?
 
-    # Special cases. Only required for performance enhancement.
-    return self.clone  if other.one?
-    return other.clone if self.one?
-
-    new_label      = "#{label.to_s}_#{other.label.to_s}".to_sym
-    new_name       = "#{name} #{other.name}"
-    new_symbol     = "#{symbol} . #{other.symbol}"
     new_dimensions = dimensions.merge(other.dimensions) do |_, pow, other_pow|
       pow + other_pow
     end.delete_if{ |_, pow| pow == 0 }
-
-    Dimension.new new_label, name: new_name, symbol: new_symbol, dimensions: new_dimensions
-  end
-
-  def / other
-    other = Dimension.one if other.is_a? Numeric
-    raise TypeError unless other.is_a? Dimension
-
-    # Special cases. Only required for performance enhancement.
-    return self.clone       if other.one?
-    return other.reciprocal if self.one?
-
-    new_label      = "#{label.to_s}_per_#{other.label.to_s}".to_sym
-    new_name       = "#{name} per #{other.name}"
-    new_symbol     = "#{symbol} / #{other.symbol}"
-    new_dimensions = dimensions.merge(other.reciprocal_dimensions) do |_, pow, other_pow|
-      pow + other_pow
-    end.delete_if{ |_, pow| pow == 0 }
-
-    Dimension.new new_label, name: new_name, symbol: new_symbol, dimensions: new_dimensions
+    Dimension.new dimensions: new_dimensions
   end
 
   def ** other
     raise TypeError unless other.is_a? Numeric
-
-    # Special cases. Only required for performance enhancement.
     return self.clone      if other ==  1
     return self.reciprocal if other == -1
     return Dimension.one   if other ==  0 || self.one?
 
-    new_label      = "#{label.to_s}_#{other.to_s}".to_sym
-    new_name       = "#{name} raised to power #{other.to_s}"
-    new_symbol     = "#{symbol}^#{other.to_s}"
     new_dimensions = Hash[dimensions.map{ |dim, pow| [dim, (pow * other).to_i] }].delete_if{ |_, pow| pow == 0 }
-
-    Dimension.new new_label, name: new_name, symbol: new_symbol, dimensions: new_dimensions
-
+    Dimension.new dimensions: new_dimensions
   end
-
 
   def coerce other
     raise TypeError, "#{other.class} can't be coerced to Dimension" unless other.is_a? Numeric
     return Dimension.one, self
-  end
-
-  def reciprocal_dimensions
-    Hash[dimensions.map{ |k, v| [k, v*-1] }]
-  end
-
-  def reciprocal
-    Dimension.new "per_#{label}".to_sym, 
-                  name: "per #{name}", 
-                  symbol: "1 / #{symbol}", 
-                  dimensions: reciprocal_dimensions
   end
 
   #
@@ -130,5 +94,4 @@ class Dimension
   def self.one
     Dimension.new :one, symbol: '1', dimensions: {}
   end
-
 end
