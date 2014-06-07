40ebab5b7cb1958e096adfd7bed3d49cb5d4708b
Luis Bacelar <lmbacelar@gmail.com>
2014-06-07 22:42:50 +0100

    Added methods for adding prefixes and units to the unit system. Added SI templates.

--- system_of_units.rb
+++ system_of_units.rb
@@ -7,4 +7,87 @@ class SystemOfUnits
     @units                = options.fetch(:units)                { [] }
     @system_of_quantities = options.fetch(:system_of_quantities)
   end
+
+  def base_units
+    units.map { |u| u.base? }
+  end
+
+  def add_prefix label_or_prefix, symbol=nil, factor=nil, options={}
+    @prefixes << case label_or_prefix
+      when Prefix then label_or_prefix
+      else Prefix.new label, symbol, factor, options
+    end
+    @prefixes.last
+  end
+
+  def add_unit unit_or_label, symbol=nil, factor=nil, quantity=nil, options={}
+    @units << case unit_or_label
+      when Unit then unit_or_label
+      else
+        quantity = system_of_quantities.quantity_for(quantity) unless quantity.is_a? Quantity
+        Unit.new label, symbol, factor, quantity, options
+    end
+    @units.last
+  end
+
+  def configure(&block)
+    instance_eval(&block) if block
+  end
+
+  def load_si
+    @system_of_quantities = SystemOfQuantities.new(:isq, name: 'International System of Quantities').load_isq
+    load_si_prefixes
+    load_si_units
+  end
+
+  def load_si_prefixes
+    self.configure do
+      add_prefix :yotta, 'Y',  10**24
+      add_prefix :zetta, 'Z',  10**21
+      add_prefix :exa,   'E',  10**18
+      add_prefix :peta,  'P',  10**15
+      add_prefix :tera,  'T',  10**12
+      add_prefix :giga,  'G',  10**9
+      add_prefix :mega,  'M',  10**6
+      add_prefix :kilo,  'k',  10**3
+      add_prefix :hecto, 'h',  10**2
+      add_prefix :deca,  'da', 10**1
+      add_prefix :deci,  'd',  10**-1
+      add_prefix :centi, 'c',  10**-2
+      add_prefix :mili,  'm',  10**-3
+      add_prefix :micro, 'µ',  10**-6
+      add_prefix :nano,  'n',  10**-9
+      add_prefix :pico,  'p',  10**-12
+      add_prefix :femto, 'f',  10**-15
+      add_prefix :atto,  'a',  10**-18
+      add_prefix :zepto, 'z',  10**-21
+      add_prefix :yocto, 'y',  10**-24
+
+      add_prefix :yobi,  'Yi', 2**80
+      add_prefix :zebi,  'Zi', 2**70
+      add_prefix :exbi,  'Ei', 2**60
+      add_prefix :pebi,  'Pi', 2**50
+      add_prefix :tebi,  'Ti', 2**40
+      add_prefix :gibi,  'Gi', 2**30
+      add_prefix :mebi,  'Mi', 2**20
+      add_prefix :kibi,  'Ki', 2**10
+    end
+    return self
+  end
+
+  #
+  # TODO: Add remaining units
+  #
+  def load_si_units
+    self.configure do
+      add_unit :meter,    'm',   1.0, :length
+      add_unit :kilogram, 'kg',  1.0, :mass
+      add_unit :second,   's',   1.0, :time
+      add_unit :ampere,   'A',   1.0, :electric_current,    name: 'Ampere'
+      add_unit :kelvin,   'K',   1.0, :temperature,         name: 'Kelvin'
+      add_unit :mole,     'mol', 1.0, :amount_of_substance
+      add_unit :candela,  'cd',  1.0, :luminous_intensity
+    end
+    return self
+  end
 end
--- system_of_units_spec.rb
+++ system_of_units_spec.rb
@@ -1,39 +1,51 @@
 require_relative 'system_of_units'
+require_relative 'unit'
+require_relative 'prefix'
+require_relative 'system_of_quantities'
+require_relative 'quantity'
 
 describe SystemOfUnits do
 
-  let(:soq) { double }
-  let(:sou) { SystemOfUnits.new(:sou,   system_of_quantities: soq) }
-  let(:cgs) { SystemOfUnits.new(:c_g_s, system_of_quantities: soq) }
-  let(:si ) { SystemOfUnits.new(:isq, name: 'International System of Units').load_si }
+  let(:soq   ) { double }
+  let(:sou   ) { SystemOfUnits.new :sou, system_of_quantities: soq }
+  let(:si    ) { SystemOfUnits.new(:isq, name: 'International System of Units', system_of_quantities: nil).load_si }
+  let(:length) { Quantity.new :length }
+  let(:meter ) { Unit.new :meter, 'm', 1.0, length }
+  let(:mili  ) { Prefix.new :mili, 'm', 1.0e-3 }
 
   context 'Instance' do
     context 'Creation' do
       it 'should have a label, a name, a collection of prefixes, a collection of units and a system of quantities' do
-        expect(cgs.label               ).to eq :c_g_s
-        expect(cgs.name                ).to eq 'c g s'
-        expect(cgs.prefixes            ).to respond_to :each
-        expect(cgs.units               ).to respond_to :each
-        expect(cgs.system_of_quantities).to eq soq
+        expect(sou.label               ).to eq :sou
+        expect(sou.name                ).to eq 'sou'
+        expect(sou.prefixes            ).to respond_to :each
+        expect(sou.units               ).to respond_to :each
+        expect(sou.system_of_quantities).to eq soq
       end
 
-      # it 'allows individual addition of quantities' do
-      #   expect{ soq.add :mass,   nil,    symbol: 'M' }.to change{ soq.quantities.count }.by 1
-      #   expect{ soq.add :length, length, symbol: 'L' }.to change{ soq.quantities.count }.by 1
-      #   expect{ soq.add :radius, length              }.to change{ soq.quantities.count }.by 1
-      # end
+      it 'allows individual addition of prefixes' do
+        expect{ sou.add_prefix :mili, 'm', 1.0e-3 }.to change{ sou.prefixes.count }.by 1
+        expect{ sou.add_prefix mili }.to change{ sou.prefixes.count }.by 1
+      end
 
-      # it 'allows bulk addition of quantities' do
-      #   expect {
-      #     soq.configure do
-      #       add :mass, nil, symbol: 'M'
-      #     end
-      #   }.to change{ soq.quantities.count }.by 1
-      # end
+      it 'allows bulk addition of prefixes and units' do
+        expect {
+          sou.configure do
+            add_prefix :mili,  'm', 1.0e-3
+            add_unit   :meter, 'm', 1.0, Quantity.new(:length)
+          end
+        }.to change{ sou.prefixes.count }.by 1
+      end
 
-      # it 'knows how to create itself according to the International System of Quantities' do
-      #   expect(isq.base_quantities.count).to eq 7
-      # end
+      it 'allows individual addition of units' do
+        expect{ sou.add_unit :mili, 'm', 1.0e-3, length }.to change{ sou.units.count }.by 1
+        expect{ sou.add_unit meter                      }.to change{ sou.units.count }.by 1
+      end
+
+      it 'knows how to create itself according to the International System of Units' do
+        expect(si.prefixes.count  ).to eq 28
+        expect(si.base_units.count).to be 7
+      end
     end
 
     # context 'Retrieval' do
