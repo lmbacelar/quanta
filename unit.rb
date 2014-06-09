d49f005d81ad4af69109254698ac8e5bc4961e34
Luis Bacelar <lmbacelar@gmail.com>
2014-06-09 10:14:28 +0100

    label is symbolized symbol unless overide exists.

--- unit.rb
+++ unit.rb
@@ -1,13 +1,13 @@
 class Unit
   attr_reader :label, :name, :symbol, :factor, :quantity
-  def initialize label, symbol = '', factor = 1.0, quantity = Quantity.new, options = {}
+  def initialize label, name = '', factor = 1.0, quantity = Quantity.new, options = {}
     raise TypeError, 'factor must be numeric'      unless factor.is_a?   Numeric
     raise TypeError, 'quantity must be a Quantity' unless quantity.is_a? Quantity
     @label    = label
-    @symbol   = symbol
+    @name     = name
+    @symbol   = options.fetch(symbol) { label.to_s.tr_s '_', ' ' }
     @factor   = factor
     @quantity = quantity
-    @name     = options.fetch(name) { label.to_s.tr_s '_', ' ' }
     freeze
   end
 
