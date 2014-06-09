97fd89205b6eaaddec9bb262a361b787c7369770
Luis Bacelar <lmbacelar@gmail.com>
2014-06-09 10:15:22 +0100

    label is symbolized symbol unless overide exists.

--- unit_spec.rb
+++ unit_spec.rb
@@ -4,31 +4,31 @@ require_relative 'quantity'
 describe Unit do
 
   let(:length) { Quantity.new :length }
-  let(:meter ) { Unit.new :meter, 'm', 1.0, length }
+  let(:meter ) { Unit.new :m, 'meter', 1.0, length }
   
   context 'Creation' do
     it 'should have a label, a name, a symbol, a factor and a quantity' do
-      expect(meter.label   ).to eq :meter
+      expect(meter.label   ).to eq :m
       expect(meter.name    ).to eq 'meter'
       expect(meter.symbol  ).to eq 'm'
       expect(meter.factor  ).to eq 1.0
       expect(meter.quantity).to eq length
     end
 
-    it 'defaults name to label, symbol to empty, factor to 1.0 and quantity to unity' do
+    it 'defaults name to label, symbol to label, factor to 1.0 and quantity to unity' do
       unit = Unit.new :a_label
-      expect(unit.name    ).to eq 'a label'
-      expect(unit.symbol  ).to eq ''
+      expect(unit.name    ).to eq ''
+      expect(unit.symbol  ).to eq 'a label'
       expect(unit.factor  ).to eq 1.0
       expect(unit.quantity).to eq Quantity.new
     end
 
     it 'raises Type Error for non numeric factor' do
-      expect{ Unit.new :meter, 'm', :non_numeric }.to raise_error TypeError
+      expect{ Unit.new :m, 'meter', :non_numeric }.to raise_error TypeError
     end
     
     it 'raises Type Error for non Quantity quantity' do
-      expect{ Unit.new :meter, 'm', 1.0, :non_quantity }.to raise_error TypeError
+      expect{ Unit.new :m, 'meter', 1.0, :non_quantity }.to raise_error TypeError
     end
 
     it 'should be immutable' do
