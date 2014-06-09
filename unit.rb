701e145491f2c464ec222f2f317c4e409a07fc39
Luis Bacelar <lmbacelar@gmail.com>
2014-06-09 15:38:47 +0100

    Renaming factor => factors. Factors holds array with polynomial conversion coefficients to base units. Added #derived? method (derived is not base).

--- unit.rb
+++ unit.rb
@@ -1,17 +1,23 @@
 class Unit
-  attr_reader :label, :name, :symbol, :factor, :quantity
-  def initialize label, name = '', factor = 1.0, quantity = Quantity.new, options = {}
-    raise TypeError, 'factor must be numeric'      unless factor.is_a?   Numeric
+  attr_reader :label, :name, :symbol, :factors, :quantity
+  def initialize label, name = '', factors = [1.0, 0.0], quantity = Quantity.new, options = {}
+    factors = Array(factors)
+    factors << 0.0 if factors.count == 1
+    factors.each{ |factor| raise TypeError, 'factor must be numeric' unless factor.is_a? Numeric }
     raise TypeError, 'quantity must be a Quantity' unless quantity.is_a? Quantity
     @label    = label
     @name     = name
     @symbol   = options.fetch(symbol) { label.to_s.tr_s '_', ' ' }
-    @factor   = factor
+    @factors  = factors
     @quantity = quantity
     freeze
   end
 
   def base?
-    quantity.base? && factor == 1.0
+    quantity.base? && factors == [1.0, 0.0]
+  end
+
+  def derived?
+    !base?
   end
 end
--- unit_spec.rb
+++ unit_spec.rb
@@ -4,27 +4,28 @@ require_relative 'quantity'
 describe Unit do
 
   let(:length) { Quantity.new :length }
-  let(:meter ) { Unit.new :m, 'meter', 1.0, length }
+  let(:meter ) { Unit.new :m, 'meter', [1.0, 0.0], length }
   
   context 'Creation' do
-    it 'should have a label, a name, a symbol, a factor and a quantity' do
+    it 'should have a label, a name, a symbol, a factors array and a quantity' do
       expect(meter.label   ).to eq :m
       expect(meter.name    ).to eq 'meter'
       expect(meter.symbol  ).to eq 'm'
-      expect(meter.factor  ).to eq 1.0
+      expect(meter.factors ).to eq [1.0, 0.0]
       expect(meter.quantity).to eq length
     end
 
-    it 'defaults name to label, symbol to label, factor to 1.0 and quantity to unity' do
+    it 'defaults name to label, symbol to label, factors array to [1.0, 0.0] and quantity to base' do
       unit = Unit.new :a_label
       expect(unit.name    ).to eq ''
       expect(unit.symbol  ).to eq 'a label'
-      expect(unit.factor  ).to eq 1.0
+      expect(unit.factors ).to eq [1.0, 0.0]
       expect(unit.quantity).to eq Quantity.new
     end
 
-    it 'raises Type Error for non numeric factor' do
-      expect{ Unit.new :m, 'meter', :non_numeric }.to raise_error TypeError
+    it 'raises Type Error for non numeric factors' do
+      expect{ Unit.new :u, 'unit', :non_numeric        }.to raise_error TypeError
+      expect{ Unit.new :u, 'unit', [1.0, :non_numeric] }.to raise_error
     end
     
     it 'raises Type Error for non Quantity quantity' do
@@ -42,6 +43,10 @@ describe Unit do
     context 'Inspection' do
       it 'knows if it is a base unit' do
         expect(meter).to be_base
+      end
+
+      it 'knows if it is a derived unit' do
+        expect(meter).not_to be_derived
       end
     end
   end
