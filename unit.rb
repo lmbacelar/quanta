135baa6cae2833e7585655b8e99833ee7809c2cb
Luis Bacelar <lmbacelar@gmail.com>
2014-06-09 19:08:10 +0100

    Using factor, scale instead of array of factors. Added Comparison, Inspection and Multiplication methods.

--- unit.rb
+++ unit.rb
@@ -1,23 +1,52 @@
 class Unit
-  attr_reader :label, :name, :symbol, :factors, :quantity
-  def initialize label, name = '', factors = [1.0, 0.0], quantity = Quantity.new, options = {}
-    factors = Array(factors)
-    factors << 0.0 if factors.count == 1
-    factors.each{ |factor| raise TypeError, 'factor must be numeric' unless factor.is_a? Numeric }
-    raise TypeError, 'quantity must be a Quantity' unless quantity.is_a? Quantity
+  include Comparable
+
+  attr_reader :label, :name, :symbol, :factor, :scale, :quantity
+  def initialize label, name = '', factor = 1.0, quantity = Quantity.new, options = {}
+    raise TypeError, 'factor must be numeric'      unless factor.is_a?   Numeric
+    raise TypeError, 'quantity must be a quantity' unless quantity.is_a? Quantity
     @label    = label
     @name     = name
-    @symbol   = options.fetch(symbol) { label.to_s.tr_s '_', ' ' }
-    @factors  = factors
+    @symbol   = options.fetch(:symbol) { label.to_s.tr_s '_', ' ' }
+    @factor   = factor
+    @scale    = options.fetch(:scale)  { 0.0 }
+    raise TypeError, 'scale must be numeric'       unless scale.is_a?    Numeric
     @quantity = quantity
     freeze
   end
 
   def base?
-    quantity.base? && factors == [1.0, 0.0]
+    quantity.base? && factor == 1.0 && scale == 0.0
   end
 
   def derived?
     !base?
   end
+
+  def to_s
+    label.to_s
+  end
+
+  def same_kind_as? other
+    quantity.same_kind_as? other.quantity
+  end
+
+  def <=> other
+    other.is_a?(Unit) && quantity.same_kind_as?(other.quantity) && factor <=> other.factor
+  end
+
+  def hash
+    [Unit, label, name, symbol, factor, scale, quantity].hash
+  end
+  alias_method :eql?, :==
+
+  def * other
+    label   = "#{label.to_s} #{other.label.to_s}".to_sym
+    name    = "#{name} #{other.name}"
+    debugger
+    fct     = factor * other.factor
+    qty     = quantity * other.quantity
+    Unit.new label, name, fct, qty
+  end
+
 end
--- unit_spec.rb
+++ unit_spec.rb
@@ -1,33 +1,50 @@
 require_relative 'unit'
 require_relative 'quantity'
+require 'debugger'
 
 describe Unit do
 
-  let(:length) { Quantity.new :length }
-  let(:meter ) { Unit.new :m, 'meter', [1.0, 0.0], length }
+  let(:length     ) { Quantity.new :length                                            }
+  let(:time       ) { Quantity.new :time                                              }
+  let(:temperature) { Quantity.new :temperature                                    }
+  let(:area       ) { Quantity.new :area, { length => 2 }                             }
+  let(:one        ) { Quantity.one                                                    }
+  let(:meter      ) { Unit.new :m,   'meter',  1.0,        length                     }
+  let(:inch       ) { Unit.new :in,  'inch',   0.0254,     length                     }
+  let(:mile       ) { Unit.new :mi,  'mile',   1.609344e3, length                     }
+  let(:are        ) { Unit.new :a,   'are',    100.0,      area                       }
+  let(:second     ) { Unit.new :s,   'second', 1.0,        time                       }
+  let(:kelvin     ) { Unit.new :K,   'Kelvin', 1.0,        temperature, scale: 273.15 }
+  let(:unitless   ) { Unit.new :one, '',       1.0,        time                       }
+  let(:percent    ) { Unit.new :pct, '%',      1.0e-2,     time                       }
   
   context 'Creation' do
-    it 'should have a label, a name, a symbol, a factors array and a quantity' do
-      expect(meter.label   ).to eq :m
-      expect(meter.name    ).to eq 'meter'
-      expect(meter.symbol  ).to eq 'm'
-      expect(meter.factors ).to eq [1.0, 0.0]
-      expect(meter.quantity).to eq length
+    it 'should have a label, a name, a symbol, a factor, a scale and a quantity' do
+      expect(kelvin.label   ).to eq :K
+      expect(kelvin.name    ).to eq 'Kelvin'
+      expect(kelvin.symbol  ).to eq 'K'
+      expect(kelvin.factor  ).to eq 1.0
+      expect(kelvin.scale   ).to eq 273.15
+      expect(kelvin.quantity).to eq temperature
     end
 
-    it 'defaults name to label, symbol to label, factors array to [1.0, 0.0] and quantity to base' do
+    it 'defaults name to label, symbol to label, factor to 1.0, scale to 0.0 and quantity to base' do
       unit = Unit.new :a_label
       expect(unit.name    ).to eq ''
       expect(unit.symbol  ).to eq 'a label'
-      expect(unit.factors ).to eq [1.0, 0.0]
+      expect(unit.factor  ).to eq 1.0
+      expect(unit.scale   ).to eq 0.0
       expect(unit.quantity).to eq Quantity.new
     end
 
-    it 'raises Type Error for non numeric factors' do
-      expect{ Unit.new :u, 'unit', :non_numeric        }.to raise_error TypeError
-      expect{ Unit.new :u, 'unit', [1.0, :non_numeric] }.to raise_error
+    it 'raises Type Error for non numeric factor' do
+      expect{ Unit.new :u, 'unit', :non_numeric }.to raise_error TypeError
     end
     
+    it 'raises Type Error for non numeric scale' do
+      expect{ Unit.new :u, 'unit', 1.0, length, scale: :non_numeric }.to raise_error TypeError
+    end
+
     it 'raises Type Error for non Quantity quantity' do
       expect{ Unit.new :m, 'meter', 1.0, :non_quantity }.to raise_error TypeError
     end
@@ -37,17 +54,117 @@ describe Unit do
       expect{ meter.instance_variable_set :@name,      0 }.to raise_error
       expect{ meter.instance_variable_set :@symbol,    0 }.to raise_error
       expect{ meter.instance_variable_set :@factor,    0 }.to raise_error
+      expect{ meter.instance_variable_set :@scale,     0 }.to raise_error
       expect{ meter.instance_variable_set :@quantity,  0 }.to raise_error
     end
+  end
 
-    context 'Inspection' do
-      it 'knows if it is a base unit' do
-        expect(meter).to be_base
-      end
-
-      it 'knows if it is a derived unit' do
-        expect(meter).not_to be_derived
-      end
+  context 'Inspection' do
+    it 'knows if it is a base unit' do
+      expect(meter).to be_base
     end
+
+    it 'knows if it is a derived unit' do
+      expect(meter).not_to be_derived
+    end
+
+    it 'knows if it has the same kind as other unit' do
+      expect(meter).to     be_same_kind_as inch
+      expect(meter).not_to be_same_kind_as second
+    end
+  end
+
+  context 'Output' do
+    it 'converts itself to String' do
+      expect(meter.to_s).to eq 'm'
+    end
+    
+    # TODO: Compound units string conversion
+  end
+
+  context 'Comparison' do
+    it 'can compare itself to other units of the same quantity' do
+      other_meter = meter.clone
+      expect(meter <=> inch       ).to eq  1 
+      expect(meter <=> other_meter).to eq  0
+      expect(meter <=> mile       ).to eq -1 
+    end
+
+    it 'can compare itself to other units of different quantity' do
+      expect(meter <=> second).to eq false
+    end
+
+    it 'knows if it is between other units' do
+      expect(meter.between? inch, mile).to be_true
+    end
+
+    it 'implements hash equality based on label, name, symbol, factors and quantity' do
+      a_hash = { meter => :hash_value }
+      expect(a_hash[meter.clone]).to eq :hash_value
+    end
+
+    # TODO: Compound units comparison
+  end
+
+  context 'Multiplication and Division' do
+    it 'can multiply itself by other unit' do
+      expect(meter * meter).to be_same_kind_as are
+      # expect(length * one ).to be_same_kind_as length
+    end
+
+    # it 'can multiply itself by a numeric and vice-versa' do
+    #   expect(length * 2).to eq length
+    #   expect(2 * length).to eq length
+    # end
+
+    # it 'defines multiplication as cumutative' do
+    #   expect(area * length  ).to be_same_kind_as(length * area)
+    #   expect(velocity * time).to be_same_kind_as(time * velocity)
+    # end
+
+    # it 'can divide itself by other quantity' do
+    #   expect(length / time       ).to be_same_kind_as velocity
+    #   expect(area / one).to be_same_kind_as area
+    #   expect(one / time).to be_same_kind_as frequency
+    # end
+
+    # it 'can divide itself by a numeric, and vice-versa' do
+    #   expect(time / 1).to be_same_kind_as time
+    #   expect(1 / time).to be_same_kind_as frequency
+
+    # end
+
+    # it 'raises Type Error when multiplied by a non Numeric or non Quantity' do
+    #   expect{ length * 'not a quantity' }.to raise_error TypeError
+    # end
+
+    # it 'raises Type Error when divided by a non Numeric or non Quantity' do
+    #   expect{ length / 'not a quantity' }.to raise_error TypeError
+    # end
+
+    # it 'can power itself to positive integers' do
+    #   expect(length ** 2).to be_same_kind_as area
+    #   expect(length ** 1).to be_same_kind_as length
+    #   expect(one ** 2   ).to be_same_kind_as one
+    # end
+
+    # it 'can power itself to zero' do
+    #   expect(length ** 0).to be_same_kind_as one
+    #   expect(one ** 0   ).to be_same_kind_as one
+    # end
+
+    # it 'can power itself to negative integers' do
+    #   expect(time ** -1).to be_same_kind_as frequency
+    #   expect(one ** -1 ).to be_same_kind_as one
+    # end
+
+    # it 'can power itself to floats' do
+    #   expect(area ** 0.5).to be_same_kind_as length
+    # end
+
+    # it 'raises Type Error when powered to non Numeric' do
+    #   expect{ length ** 'not a number' }.to raise_error TypeError
+    #   expect{ length ** length         }.to raise_error TypeError
+    # end
   end
 end
