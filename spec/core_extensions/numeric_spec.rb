701fbd54712b12a2c3fa6167149b4349c14ad0cb
Luis Bacelar <lmbacelar@gmail.com>
2014-07-09 01:58:12 +0100

    corrected specs assuring Numerics given are not Integers

--- spec/core_extensions/numeric_spec.rb
+++ spec/core_extensions/numeric_spec.rb
@@ -4,10 +4,10 @@ describe Numeric do
   context 'Methods' do
     context '#to_superscript' do
       it 'returns string representation of numeric, prepended with character ^' do
-        expect(   0.to_superscript).to eq '^0'
-        expect(   1.to_superscript).to eq '^1'
+        expect( 0.0.to_superscript).to eq '^0.0'
+        expect( 1.0.to_superscript).to eq '^1.0'
         expect( 4.2.to_superscript).to eq '^4.2'
-        expect(  -1.to_superscript).to eq '^-1'
+        expect(-1.0.to_superscript).to eq '^-1.0'
         expect(-4.2.to_superscript).to eq '^-4.2'
       end
     end
