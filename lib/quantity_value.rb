1d971a3a83cedd2c00f08894a25fcc2284256901
Luis Bacelar <lmbacelar@gmail.com>
2014-07-11 18:16:43 +0100

    using self.class instead of hardcoded classname. memoized #hash method

--- lib/quantity_value.rb
+++ lib/quantity_value.rb
@@ -1,11 +1,12 @@
 class QuantityValue
   include Comparable
 
-  attr_reader :value, :unit
+  attr_reader :value, :unit, :hash
   def initialize value, unit, prefix = nil
     raise TypeError, 'value has to be numeric' unless value.is_a? Numeric
     @value  = value.to_f
     @unit   = unit
+    @hash   = [self.class, value, unit].hash
     freeze
   end
 
@@ -22,14 +23,11 @@ class QuantityValue
   end
 
   def <=> other
-    other.is_a?(QuantityValue) && 
+    other.is_a?(self.class) && 
     unit.same_kind_as?(other.unit) && 
     base_value <=> other.base_value
   end
 
-  def hash
-    [QuantityValue, value, unit].hash
-  end
   alias_method :eql?, :==
 
   def + other
@@ -49,23 +47,23 @@ class QuantityValue
   end
 
   def coerce other
-    raise TypeError, "#{other.class} can't be coerced to QuantityValue" unless other.is_a? Numeric
-    return QuantityValue.new(other.to_f, Unit::Plain.unitless), self
+    raise TypeError, "#{other.class} can't be coerced to #{self.class}" unless other.is_a? Numeric
+    return self.class.new(other.to_f, Unit::Plain.unitless), self
   end
 
 protected
   def add_or_subtract operator, other
-    raise TypeError unless other.is_a? QuantityValue
+    raise TypeError unless other.is_a? self.class
     raise TypeError unless unit.same_kind_as? other.unit
     result = (base_value.send operator, other.base_value) / unit.factor
-    QuantityValue.new result, unit
+    self.class.new result, unit
   end
 
   def multiply_or_divide operator, other
-    other = QuantityValue.new(other.to_f, Unit::Plain.unitless) if other.is_a? Numeric
-    raise TypeError unless other.is_a? QuantityValue
+    other = self.class.new(other.to_f, Unit::Plain.unitless) if other.is_a? Numeric
+    raise TypeError unless other.is_a? self.class
     result_value = value.send operator, other.value
     result_unit  = unit.send  operator, other.unit
-    QuantityValue.new result_value, result_unit
+    self.class.new result_value, result_unit
   end
 end
