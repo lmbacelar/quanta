7ded49e7ae4c32ba9db288fc909d836b3cef3f39
Luis Bacelar <lmbacelar@gmail.com>
2014-07-15 20:24:37 +0100

    added #numerator, #denominator to extract from list of units. added #label, #name methods to derive attributes from list of units

--- spec/si/unit/list_spec.rb
+++ spec/si/unit/list_spec.rb
@@ -4,8 +4,26 @@ module SI
   module Unit
     describe List do
       include_context :unit_examples
+      let(:units                 ) { units = [{ kilogram =>  1 }, { metre => 1 }, { second   => -2 }] }
+      let(:shuffled_units        ) { units = [{ second   => -2 }, { metre => 1 }, { kilogram => 1  }] }
+      let(:numerator_only_units  ) { units = [{ kilogram =>  1 }, { metre => 1 }                    ] }
+      let(:denominator_only_units) { units = [                                    { second   => -2 }] }
 
       context 'Creation' do
+        it 'derives label from labels of units' do
+          expect( units.extend(List).label                  ).to eq :"kg.m/s²"
+          expect( shuffled_units.extend(List).label         ).to eq :"kg.m/s²"
+          expect( numerator_only_units.extend(List).label   ).to eq :"kg.m"
+          expect( denominator_only_units.extend(List).label ).to eq :"/s²"
+        end
+
+        it 'derives name from names of units' do
+          expect( units.extend(List).name                 ).to eq 'kilogram metre per second squared'
+          expect( shuffled_units.extend(List).name        ).to eq 'kilogram metre per second squared'
+          expect( numerator_only_units.extend(List).name  ).to eq 'kilogram metre'
+          expect( denominator_only_units.extend(List).name).to eq 'per second squared'
+        end
+
         it 'derives factor from factors of units' do
           expect(kilometre_per_hour.factor).to eq 1000.0/3600.0
         end
@@ -17,6 +35,14 @@ module SI
       end
 
       context 'Inspection' do
+        it 'retrieves numerator units from list' do
+          expect( units.extend(List).numerator ).to eq [{ kilogram => 1 }, { metre => 1 }]
+        end
+
+        it 'retrieves denominator units from list' do
+          expect( units.extend(List).denominator ).to eq [{ second => -2 }]
+        end
+
         it 'is valid for Enumerable of hashes of Unit => Numeric' do
           expect( [{ unitless => 1 }].extend(List)  ).to be_valid
           expect( [{ metre => 1 }, { metre => -1 }].extend(List)  ).to be_valid
