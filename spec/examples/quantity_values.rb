ce1db762aec01150ee602bb820ddd65fcdc3de39
Luis Bacelar <lmbacelar@gmail.com>
2014-07-15 20:35:24 +0100

    QuantityValue examples are now tied to SI loaded units

--- spec/examples/quantity_values.rb
+++ spec/examples/quantity_values.rb
@@ -1,25 +1,33 @@
-require_relative 'si/units'
+require_relative '../../lib/isq'
+require_relative '../../lib/si/unit/common'
+require_relative '../../lib/si/unit/arithmetic'
+require_relative '../../lib/si'
 require_relative '../../lib/quantity_value'
+require_relative 'si/units'
 
 RSpec.shared_context :quantity_value_examples do
+  SI.load!
+
   include_context :unit_examples
 
-  let(:zero                 ) { QuantityValue.new 0, unitless          }
-  let(:one                  ) { QuantityValue.new 1, unitless          }
-  let(:two                  ) { QuantityValue.new 2, unitless          }
-  let(:zero_metres          ) { QuantityValue.new 0, metre             }
-  let(:one_metre            ) { QuantityValue.new 1, metre             }
-  let(:other_one_metre      ) { QuantityValue.new 1, metre             }
-  let(:two_metres           ) { QuantityValue.new 2, metre             }
-  let(:three_metres         ) { QuantityValue.new 3, metre             }
-  let(:one_milimetre        ) { QuantityValue.new 1, milimetre         }
-  let(:one_kilometre        ) { QuantityValue.new 1, kilometre         }
-  let(:one_microinch        ) { QuantityValue.new 1, microinch         }
-  let(:one_second           ) { QuantityValue.new 1, second            }
-  let(:one_hertz            ) { QuantityValue.new 1, hertz             }
-  let(:one_squared_metre    ) { QuantityValue.new 1, squared_metre     }
-  let(:one_squared_kilometre) { QuantityValue.new 1, squared_kilometre }
-  let(:one_metre_second     ) { QuantityValue.new 1, metre_second      }
-  let(:one_metre_per_second ) { QuantityValue.new 1, metre_per_second  }
+  let(:zero                 ) { QuantityValue.new 0, :""                }
+  let(:one                  ) { QuantityValue.new 1, :""                }
+  let(:two                  ) { QuantityValue.new 2, :""                }
+  let(:zero_metres          ) { QuantityValue.new 0, :m                 }
+  let(:one_metre            ) { QuantityValue.new 1, :m                 }
+  let(:one_kilogram         ) { QuantityValue.new 1, :kg                }
+  let(:one_second           ) { QuantityValue.new 1, :s                 }
+  let(:other_one_metre      ) { QuantityValue.new 1, :m                 }
+  let(:two_metres           ) { QuantityValue.new 2, :m                 }
+  let(:three_metres         ) { QuantityValue.new 3, :m                 }
+  let(:one_milimetre        ) { QuantityValue.new 1, :mm                }
+  let(:one_kilometre        ) { QuantityValue.new 1, :km                }
+  let(:one_microinch        ) { QuantityValue.new 1, :µin               }
+  let(:one_second           ) { QuantityValue.new 1, :s                 }
+  let(:one_hertz            ) { QuantityValue.new 1, :Hz                }
+  let(:one_squared_metre    ) { QuantityValue.new 1, :m²                }
+  let(:one_squared_kilometre) { QuantityValue.new 1, :km²               }
+  let(:one_metre_second     ) { QuantityValue.new 1, :"m.s"             }
+  let(:one_metre_per_second ) { QuantityValue.new 1, :"m/s"             }
 end
 
